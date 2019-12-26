e6a621ed6aa19137306feef443021f38d29c8cd6
MihailRozhnovskiy <mihail.exp@gmail.com>
2019-12-26 15:53:39 +0200

    Добавляет верстку главной страницы (кроме слайдеров)

--- build/js/slider.js
+++ build/js/slider.js
@@ -1,12 +1,2 @@
 
-var slider = new Slider('.bikes__slider', {
-  slidesPerViw: 3,
-  spaseBetween: 30,
-  slidesPerGroup: 3,
-  loop: true,
-  loopFillGroupWithBlank: true,
-  navigation: {
-    nextEl: '.bikes__slider-button-next',
-    prevEl: '.bikes__slider-button-prev',
-  },
-});
+
